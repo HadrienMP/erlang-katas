54b76fef6cb20863ef3484fbe78b99bb2e43e76c
HadrienMP <public@hadrienmp.fr>
2017-05-20 15:20:15 +0200

    Smaller and slower

--- src/fizzbuzz.erl
+++ src/fizzbuzz.erl
@@ -13,13 +13,9 @@
 -export([fizzbuzz/1]).
 
 
-fizzbuzz(X) -> fizzbuzz(X, fizz(X) ++ buzz(X)).
+fizzbuzz(X) -> fizzbuzz(X, "", "").
 
-fizzbuzz(X, "") -> erlang:integer_to_list(X);
-fizzbuzz(_, Result) -> Result.
-
-fizz(X) when X rem 3 =:= 0 -> "fizz";
-fizz(_) -> "".
-
-buzz(X) when X rem 5 =:= 0 -> "buzz";
-buzz(_) -> "".
+fizzbuzz(X, _, Buzz) when X rem 3 =:= 0 -> fizzbuzz(X div 3, "fizz", Buzz);
+fizzbuzz(X, Fizz, _) when X rem 5 =:= 0 -> fizzbuzz(X div 5, Fizz, "buzz");
+fizzbuzz(X, "", "") -> erlang:integer_to_list(X);
+fizzbuzz(_, Fizz, Buzz) -> Fizz ++ Buzz.
