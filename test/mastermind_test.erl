7768bea05b38a8736eee280d59cead4631cc600a
hadrienmp <public@hadrienmp.fr>
2020-05-17 09:49:47 +0200

    Refactor tests

--- test/mastermind_test.erl
+++ test/mastermind_test.erl
@@ -4,11 +4,15 @@
 %% API
 -export([]).
 
-single_color_exact_match_test() -> ?assertEqual([1,0], compare([blue], [blue])).
-single_color_no_match_test() -> ?assertEqual([0,0], compare([red], [blue])).
-exact_match_test() -> ?assertEqual([2,0], compare([blue, red], [blue, red])).
+'an exact match is the right color at the right place_test'() ->
+    [
+     ?assertMatch([1,_], compare([blue, green ,red], [yellow, green, purple])),
+     ?assertMatch([2,_], compare([blue, red], [blue, red])),
+     ?assertMatch([1,_], compare([blue], [blue])),
+     ?assertMatch([0,_], compare([blue, green], [red, yellow]))
+    ].
 
-compare(Guess, Secret) -> [exact_matches(0, Guess, Secret),0].
+compare(Guess, Secret) -> [exact_matches(0, Guess, Secret), 0].
 
 exact_matches(Matches, [], []) -> Matches;
 exact_matches(Matches, [Color | Guess], [Color | Secret]) -> exact_matches(Matches + 1, Guess, Secret);
